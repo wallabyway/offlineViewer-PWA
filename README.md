59d3cf4b4310828d8a6ff10e29dc2e71a714d389
Michael Beale <michael.beale@autodesk.com>
2018-09-18 15:17:59 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # offlineViewer-PWA
 offline Forge Viewer demonstrating PWA ServiceWorkers
+
+
+Web Client:
+All browser code can be found under 'docs' folder, starting with the index.html file
+
+
+Server:
+All server code can be found under 'server' folder.  Then demo uses a lambda function to generate a new access token.
+
+
+refer to https://github.com/wallabyway/viewer-serverless
---
+++ server/app.js
@@ -0,0 +1,37 @@
+'use strict'
+const express = require('express')
+const forge = require('forge-apis')
+const app = express()
+
+
+if (!process.env.FORGE_URN) {
+	console.log('Error - Please set environment variables and restart.');
+	return;
+}
+
+// gets oauth token from Forge API
+const oAuth2TwoLegged = new forge.AuthClientTwoLegged(
+	process.env.FORGE_CLIENT_ID,
+	process.env.FORGE_CLIENT_SECRET,
+	['viewables:read'])
+
+
+// The browser will ask for a fresh Access Token as a javascript file
+app.get('/js/init.js', (req, res) => {
+	oAuth2TwoLegged.authenticate().then( function(restoken) {
+		res.send(` 
+			options = {
+				accessToken: "${restoken.access_token}",
+				env: "AutodeskProduction"
+			};
+		`);
+		console.log(`Sent ForgeToken: ${restoken.access_token}`);
+	});
+})
+
+// use github to host your static website, rather than lambda
+// app.use(express.static(__dirname + '/www'));
+
+app.listen(3000); // <-- uncomment this line for local debugging, then type: >node app.js
+
+module.exports = app
